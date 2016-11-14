7f8419c778ce54a0028d1b0c2416ff7c58971f2d
ouenlue <o.unlu@outlook.com>
2016-11-14 11:49:21 +0100

    Zahlenraten mit try-catch

--- 06_ausnahmen/01_try_catch/src/tpe/exceptions/trycatch/Zahlenraten.java
+++ 06_ausnahmen/01_try_catch/src/tpe/exceptions/trycatch/Zahlenraten.java
@@ -30,8 +30,8 @@ public final class Zahlenraten {
      * @throws NumberFormatException Fehler bei der Zahlenkovertierung
      * @throws IOException I/O-Fehler
      */
-    public static void main(String[] args)
-            throws NumberFormatException, IOException {
+    public static void main(String[] args) {
+        // throws NumberFormatException, IOException {
 
         // zu ratende Zahl bestimmen
         int zahl = new Random().nextInt(100) + 1;
@@ -40,19 +40,28 @@ public final class Zahlenraten {
 
         while (true) {
             System.out.print("Bitte geben Sie eine Zahl ein: ");
-            int geraten  = Integer.parseInt(readNumber());
+            int geraten;
+
+            try {
+                geraten = Integer.parseInt(readNumber());
+                if (geraten < zahl) {
+                    System.out.println("Zu niedrig");
+                }
+                else if (geraten > zahl) {
+                    System.out.println("Zu hoch.");
+                }
+                else {
+                    System.out.printf("Richtig in %d Versuchen", versuche);
+                    break;
+                }
+            }
+            catch (NumberFormatException | IOException e) {
+                System.err.println("Falsche Eingabe!!!");
+                // e.printStackTrace();
+            }
             versuche++;
 
-            if (geraten < zahl) {
-                System.out.println("Zu niedrig");
-            }
-            else if (geraten > zahl) {
-                System.out.println("Zu hoch.");
-            }
-            else {
-                System.out.printf("Richtig in %d Versuchen", versuche);
-                break;
-            }
+
         }
     }
 }
